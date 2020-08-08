68d4746829defef5cf5e3e578172a2063b44e687
guilherme santos <guilhermess1994@gmail.com>
2020-08-08 01:41:18 -0300

    added  webfont and icons

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-
+import "@mdi/font/css/materialdesignicons.css";
 Vue.config.productionTip = false;
 
 new Vue({
