dc31a01809eaf0bbe88c00ceb38bc8a9bc5c5661
guilherme santos <guilhermess1994@gmail.com>
2020-08-08 01:42:53 -0300

    fixing uncomited organism and some structure added background assets

---
+++ src/components/organisms/OLoginGoogle.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .o-login-google
+    a-btn( label="Login with google" icon="mdi-google")
+</template>
+
+<script>
+import ABtn from "../atoms/ABtn";
+export default {
+  name: "OLoginGoogle",
+
+  components: {
+    ABtn
+  }
+};
+</script>
