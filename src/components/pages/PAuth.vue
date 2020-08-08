ac4caaac12da911fd1ab9baf1d9050d31eb9f165
guilherme santos <guilhermess1994@gmail.com>
2020-08-08 01:39:23 -0300

    created auth page, initial implementation

---
+++ src/components/pages/PAuth.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  t-auth( :backgroundUrl="backgroundUrl" )
+</template>
+
+<script>
+import TAuth from "@/components/templates/TAuth.vue";
+import backgroundUrl from "@/assets/bg2.jpg";
+
+export default {
+  name: "PLogin",
+
+  data: () => ({
+    backgroundUrl
+  }),
+
+  components: {
+    TAuth
+  }
+};
+</script>
---
+++ src/components/templates/TAuth.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  a-page.center( :backgroundUrl="backgroundUrl" )
+    .row
+      a-logo
+    .row
+      a-card
+        template(v-slot:title)
+          .row Sign in to your account
+        o-login-google.row
+</template>
+
+<script>
+import APage from "../atoms/APage";
+import ALogo from "../atoms/ALogo";
+import ACard from "../atoms/ACard";
+import OLoginGoogle from "../organisms/OLoginGoogle";
+
+export default {
+  name: "Login",
+
+  components: {
+    APage,
+    ALogo,
+    ACard,
+    OLoginGoogle
+  },
+
+  props: {
+    backgroundUrl: String
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.title
+  margin-top: 20px
+  margin-bottom: 45px
+</style>
