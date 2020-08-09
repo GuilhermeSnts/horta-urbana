0dcea385a504ec0c5d1df2a4c50dc68ccd67c5b4
guilherme santos <guilhermess1994@gmail.com>
2020-08-09 13:49:21 -0300

    created horizontal menu

---
+++ src/components/molecules/MHorizontalMenu.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  .m-horizontal-menu
+    a-tile-btn(
+        class="item"
+        :label="item.label",
+        :background="item.background",
+        :path="item.path"
+        v-for="(item, index) in items",
+        :key="index")
+</template>
+
+<script>
+import ATileBtn from "@/components/atoms/ATileBtn";
+export default {
+  name: "MHorizontalMenu",
+
+  components: {
+    ATileBtn
+  },
+
+  props: {
+    items: Array
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.m-horizontal-menu
+    width: 100%
+    height: 180px
+    overflow-x: scroll
+    display: flex
+    flex-direction: row
+    .item
+        margin: 0px 5px
+</style>
