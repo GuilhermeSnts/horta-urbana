96e2acf9942ac7a6dd9153105c6280e7fcea4bac
guilherme santos <guilhermess1994@gmail.com>
2020-08-09 00:23:58 -0300

    added firebase settings

--- src/main.js
+++ src/main.js
@@ -4,7 +4,19 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import "@mdi/font/css/materialdesignicons.css";
+import firebase from "firebase/app";
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
