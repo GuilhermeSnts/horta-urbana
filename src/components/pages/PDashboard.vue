f369ef1aed01ec3aad9d76a1c637de958b83636e
guilherme santos <guilhermess1994@gmail.com>
2020-08-09 13:49:43 -0300

    added main menu to dashboard

--- src/components/pages/PDashboard.vue
+++ src/components/pages/PDashboard.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  t-dashboard( :backgroundUrl="backgroundUrl")
+  t-dashboard( :backgroundUrl="backgroundUrl", :pagesMenu="pages")
 
 </template>
 
@@ -11,7 +11,27 @@ export default {
   name: "PHome",
 
   data: () => ({
-    backgroundUrl
+    backgroundUrl,
+    pages: [
+      {
+        label: "Gardens",
+        path: "/gardens",
+        background:
+          "https://images.unsplash.com/photo-1569677229133-730f610ae381?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+      },
+      {
+        label: "Plants",
+        path: "/plants",
+        background:
+          "https://images.unsplash.com/photo-1488353816557-87cd574cea04?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+      },
+      {
+        label: "Friends",
+        path: "/friends",
+        background:
+          "https://images.unsplash.com/photo-1500027014421-46ccc843776a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+      }
+    ]
   }),
 
   components: {
--- src/components/templates/TDashboard.vue
+++ src/components/templates/TDashboard.vue
@@ -1,20 +1,25 @@
 <template lang="pug">
   a-page.center( :backgroundUrl="backgroundUrl" )
+    div Dashboard
+    m-horizontal-menu( :items="pagesMenu")
 
 </template>
 
 <script>
 import APage from "../atoms/APage";
+import MHorizontalMenu from "../molecules/MHorizontalMenu";
 
 export default {
   name: "TDashboard",
 
   components: {
-    APage
+    APage,
+    MHorizontalMenu
   },
 
   props: {
-    backgroundUrl: String
+    backgroundUrl: String,
+    pagesMenu: Array
   }
 };
 </script>
