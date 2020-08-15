efd9c7b7360efc9e0332ca87f0b03b460113f4c3
guilherme santos <guilhermess1994@gmail.com>
2020-08-15 11:37:35 -0300

    added subtitle and caption props to a-title

--- src/components/atoms/ATitle.vue
+++ src/components/atoms/ATitle.vue
@@ -1,18 +1,35 @@
 <template lang="pug">
   .a-title
-    slot
+    .text( :class="size") 
+      slot
+    .caption(v-text="caption")
 </template>
 
 <script>
 export default {
-  name: "ATitle"
+  name: "ATitle",
+
+  props: {
+    size: {
+      type: String,
+      default: "title"
+    },
+    caption: String
+  }
 };
 </script>
 
 <style lang="sass" scoped>
 .a-title
-    font-family: 'Merriweather', serif
+  font-family: 'Merriweather', serif
+  margin-bottom: 20px
+  margin-top: 20px
+  .title
     font-weight: bold
     font-size: 1.2em
-    margin-bottom: 25px
+  .subtitle
+    font-size: 1em
+  .caption
+    font-size: 0.8em
+    color: $black-transparent
 </style>
