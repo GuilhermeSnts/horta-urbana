a46dee6ceea5bcf36d82f8c9ef44e9f96f7f1491
guilherme santos <guilhermess1994@gmail.com>
2020-08-15 11:38:45 -0300

    added title inside oplantlist

--- src/components/organisms/OPlantList.vue
+++ src/components/organisms/OPlantList.vue
@@ -1,26 +1,33 @@
 <template lang="pug">
   .o-plant-list
+    a-title(size="subtitle" :caption="caption") {{ title }}
     m-plant-item(
         v-for="(item, index) in plants",
         :key="index",
         :common_name="item.common_name",
         :scientific_name="item.scientific_name"
-        :image_url="item.image_url",
-    )
+        :image_url="item.image_url")
 </template>
 
 <script>
 import MPlantItem from "../molecules/MPlantItem.vue";
+import ATitle from "../atoms/ATitle.vue";
 
 export default {
   name: "OPlantList",
 
   components: {
-    MPlantItem
+    MPlantItem,
+    ATitle
   },
 
   props: {
-    plants: Array
+    plants: Array,
+    title: {
+      type: String,
+      default: "Plants"
+    },
+    caption: String
   }
 };
 </script>
