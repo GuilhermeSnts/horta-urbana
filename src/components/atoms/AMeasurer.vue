b176b2d90b225c4a707ccdb716fd18aad6267943
guilherme santos <guilhermess1994@gmail.com>
2020-08-24 00:21:51 -0300

    created a-measurer and added into plant-bio

---
+++ src/components/atoms/AMeasurer.vue
@@ -0,0 +1,82 @@
+<template lang="pug">
+  .a-measurer
+    i.icon.mdi(:class="icon")
+    svg(class="ring")
+      circle(
+        class="ring_circle",
+        r="60",
+        cx="50%",
+        cy="50%",
+        stroke-dasharray="270 360",
+        stroke-dashoffset="0")
+      circle(
+        class="ring_circle circle-color",
+        r="60",
+        cx="50%",
+        cy="50%",
+        :stroke-dasharray="progress+' 360'",
+        stroke-dashoffset="0",
+        :style="{stroke: color}"
+        )
+    .label {{label}}
+        
+</template>
+
+<script>
+export default {
+  name: "AMeasurer",
+  computed: {
+    progress() {
+      return this.value * 27;
+    }
+  },
+
+  props: {
+    value: Number,
+    icon: String,
+    color: String,
+    label: String
+  }
+};
+</script>
+
+<style lang="sass">
+.a-measurer
+  display: flex
+  flex-flow: column
+  width: fit-content
+  background: $white
+  justify-content: center
+  align-content: center
+  padding: 10px
+  box-sizing: border-box
+  border-radius: 10px
+  border: 1px solid grey
+  margin: 5px
+  position: relative
+  .icon
+    position: absolute
+    left: 0
+    right: 0
+    top: 50px
+    text-align: center
+    font-size: 3em
+    color: #333333b3
+  .ring
+    width: 150px
+    height: 150px
+    .ring_circle
+      fill: none
+      stroke-stroke-dashoffset: 0
+      stroke: #3333331A
+      stroke-width: 10
+      stroke-linecap: round
+      transform-origin: 50% 50%
+      transform: rotate(140deg)
+    .circle-color
+      transition: stroke-dashoffset 1s linear
+  .label
+    font-size: 1.2em
+    color: $black
+    margin: 5px
+</style>
--- src/components/templates/TPlants/TPlantBio.vue
+++ src/components/templates/TPlants/TPlantBio.vue
@@ -7,13 +7,13 @@
 
     a-title(size="subtitle") Growth
 
-    a-card( mode="flat")
-      div
-        span light
-        span {{ plant.main_species.growth.light }}
-      div
-        span Soil Nutriments: 
-        span {{ plant.main_species.growth.soil_nutriments }}
+    .row
+      a-measurer(label="light" icon="mdi-weather-sunny" color="orange" :value="plant.main_species.growth.light")
+      a-measurer(label="Soil Nutriments" icon="mdi-grass" color="brown" :value="plant.main_species.growth.soil_nutriments")
+    
+    br
+
+    a-card( mode="flat") 
       div
         span PH: 
         span {{ plant.main_species.growth.ph_maximum }} ~ 
@@ -28,6 +28,7 @@
 <script>
 import APage from "@/components/atoms/APage";
 import ATag from "@/components/atoms/ATag";
+import AMeasurer from "@/components/atoms/AMeasurer";
 import ATitle from "@/components/atoms/ATitle";
 import AImage from "@/components/atoms/AImage";
 import ACard from "@/components/atoms/ACard";
@@ -40,7 +41,8 @@ export default {
     ATitle,
     AImage,
     ACard,
-    ATag
+    ATag,
+    AMeasurer
   },
 
   props: {
@@ -49,3 +51,9 @@ export default {
   }
 };
 </script>
+
+<style lang="sass" scoped>
+.row
+  display: flex
+  flex-direction: row
+</style>
